8a0fe676169b3fce7ab2308fb465fd301dd34387
Guilherme Werner <guilherme.werner@tribufu.com>
2023-06-18 17:42:34 -0300

    Update ClientApp.java

--- client/src/main/java/com/example/redes/ClientApp.java
+++ client/src/main/java/com/example/redes/ClientApp.java
@@ -13,7 +13,7 @@ import java.net.DatagramSocket;
 import java.net.Socket;
 
 public class ClientApp extends JFrame {
-    private static final String SERVER_IP = "172.16.0.1";
+    private static final String SERVER_IP = "172.16.0.10";
     private static final int SERVER_TCP_PORT = 5475;
     private static final int SERVER_UDP_PORT = 5565;
 
